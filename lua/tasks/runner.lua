f88365276706bb1b003024a99d8ea078ec32799c
dreadster3 <afonso.antunes@live.com.pt>
2023-02-07 19:22:16 +0000

    Add debug notification

--- lua/tasks/runner.lua
+++ lua/tasks/runner.lua
@@ -186,6 +186,8 @@ function runner.chain_commands(module_type, task_name, commands, module_config, 
       local msg = "Task %s completed with success"
       local level = vim.log.levels.INFO
 
+      vim.notify(tostring(job))
+
       if job.code ~= 0 or job.signal ~= 0 then
         msg = "Task %s failed"
         level = vim.log.levels.ERROR
